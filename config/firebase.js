653b8fb98ff62fcccc75e135e3acd6aff3ed0d25
marcosgabriel <marcosgabriel.mg318@gmail.com>
2024-08-25 12:58:47 -0300

    fix:chave de credencial do firebase ajustada

--- config/firebase.js
+++ config/firebase.js
@@ -1,4 +1,4 @@
-const path = require("path");
+//const path = require("path");
 const admin = require("firebase-admin");
 
 // Caminho para o arquivo da chave do serviço do Firebase
@@ -7,18 +7,7 @@ const admin = require("firebase-admin");
 //   "project-freela-firebase-adminsdk-86npi-98e201e06c.json"
 // );
 
-const serviceAccount = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-};
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
   admin.initializeApp({
