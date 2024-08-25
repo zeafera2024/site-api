580e485260deb6857a15b32bbc6dd6e7e976da10
marcosgabriel <marcosgabriel.mg318@gmail.com>
2024-08-25 10:42:08 -0300

    change:credenciais do firebase

--- config/firebase.js
+++ config/firebase.js
@@ -1,15 +1,28 @@
 const path = require("path");
 const admin = require("firebase-admin");
 
-// Caminho para o arquivo da chave do serviço do Firebase
-const serviceAccountPath = path.resolve(
-  __dirname,
-  "project-freela-firebase-adminsdk-86npi-98e201e06c.json"
-);
+// // Caminho para o arquivo da chave do serviço do Firebase
+// const serviceAccountPath = path.resolve(
+//   __dirname,
+//   "project-freela-firebase-adminsdk-86npi-98e201e06c.json"
+// );
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountPath),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
